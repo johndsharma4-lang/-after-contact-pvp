748d132583d96bc869d38fa918c8a435ac5ff7d6
johndsharma4-lang <johndsharma4@gmail.com>
2026-08-24 08:59:35 -0700

    Refresh mobile story QA patch

--- router-worker.js
+++ router-worker.js
@@ -17,7 +17,7 @@ function documentHeaders(response) {
 
 class StoryPresentationInjector {
   element(element) {
-    element.append('<script src="/story-presentation.js?v=20260824-4"></script>', { html: true });
+    element.append('<script src="/story-presentation.js?v=20260824-5"></script>', { html: true });
   }
 }
 
